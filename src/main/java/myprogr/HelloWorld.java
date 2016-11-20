bde915b6019cea7a7b34481eb6ab0fa3111181e3
Edmund Krzeminski <e59.krzem@gmail.com>
2016-11-20 06:58:44 +0100

    fourth commit

---
+++ src/main/java/myprogr/HelloWorld.java
@@ -0,0 +1,12 @@
+package myprogr;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld init ");
+		String str ="juz zaczelo";
+		System.out.println("HelloWorld "+ str);
+	}
+
+}
