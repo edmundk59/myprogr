ad92ea8cfb03dd7f12976950204bfc6bcf4b5bf7
Edmund Krzeminski <e59.krzem@gmail.com>
2016-11-20 14:40:00 +0100

    fifth commit

--- src/main/java/myprogr/HelloWorld.java
+++ src/main/java/myprogr/HelloWorld.java
@@ -7,8 +7,10 @@ public class HelloWorld {
 		System.out.println("HelloWorld init ");
 		String str ="juz zaczelo";
 		System.out.println("HelloWorld "+ str);
-		String str2 ="poprawka";
+		String str2 ="poprawka 1";
 		System.out.println("HelloWorld "+ str2);
+		String str3 ="poprawka 2";
+		System.out.println("HelloWorld "+ str3);
 	}
 
 }
